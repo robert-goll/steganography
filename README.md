438bb43824dd78fe7e6bd59601cf7ed2a7f76ab1
Robert G <robertrover811@outlook.com>
2021-04-14 15:44:21 -0700

    started some flask stuff and in progress of making it a web app

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # steganography
 Making a steganography encoder and decoder
+
+Everyone Ignore this
+
+<!-- TODO Next week 4/21/21:
+How to set up forms in HTML
+How to show an image HTML in real time after output (prob gonna be javascript)
+handling delayed executions
+input/output images
+possible async functions 
+ -->
--- main.py
+++ main.py
@@ -1,54 +1,10 @@
-import cv2
-import numpy as np
-# from google.colab.patches import cv2_imshow
+from flask import Flask, request, render_template
 
-def messageToBinary(message):
-    if type(message) == str:
-        return "".join([format(ord(i),"08b") for i in message])
-    elif type(message) == bytes or type(message) == np.ndarray:
-        return [format(i,"08b") for i in message]
-    else:
-        raise TypeError("Input type not supported")
+app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-def hideData(image,message):
-    n_bytes = image.shape[0] * image.shape[1] * 3 // 8
-    if len(message) > n_bytes:
-        raise ValueError("Error message too long")
-    message += "#####"
-    data_index = 0
-    binary_message = messageToBinary(message)
-    data_len = len(binary_message)
-    for row in image:
-        for pixel in row:
-            if data_index >= data_len:
-                break
-            r,g,b = messageToBinary(pixel)
-            if data_index < data_len:
-                pixel[0] = int(r[:-1] + binary_message[data_index], 2)
-                data_index += 1
-            if data_index < data_len:
-                pixel[1] = int(g[:-1] + binary_message[data_index], 2)
-                data_index += 1
-            if data_index < data_len:
-                pixel[2] = int(b[:-1] + binary_message[data_index], 2)
-                data_index += 1
-    return image
-
-def showData(image):
-    binary_data = ""
-    for row in image:
-       for pixel in row:
-          r,g,b = messageToBinary(pixel)
-          binary_data += r[-1] # "01010001"[:-1] => '0101000'
-          binary_data += g[-1]
-          binary_data += b[-1]
-    all_bytes = [ binary_data[i:i+8] for i in range(len(binary_data),8)]
-    decoded_data = ""
-    for byte in all_bytes:
-        decoded_data += chr(int(byte,2))
-        if decoded_data[-5:] == "#####":
-            break
-    return decoded_data[:-5]
-
-print(messageToBinary("Hello World!"))
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='8081', debug=True)
