931d41b4c26e356c5676c6d0d12b68caf801e1ae
Robert G <robertrover811@outlook.com>
2021-04-21 15:38:06 -0700

    got rid of my virtual env then did some non python stuff

---
+++ templates/static/javascript/select.js
@@ -0,0 +1,6 @@
+console.log("hello there")
+if(window.File && window.FileReader && window.FileList && window.Blob){
+    alert("Success");
+} else{
+    alert("The File API is not supported on your browser. This app will not work.");
+}
