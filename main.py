caaf602a844316dd82b0c6c6698952ebbacaa1e3
Robert G <robertrover811@outlook.com>
2021-05-05 15:41:10 -0700

    SN15 landed and my code was right wooooooooooooooooooooooooooooooooooooooooo

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ app = Flask(__name__, static_folder="static")
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    posts = [
+        {
+            "author": {"name" : "SN15"},
+            "body": {"Hoppy is the greatest starship ever"}
+        },
+        {
+            "author": {"name" : "Hoppy"},
+            "body": {"SN15 is the greatest other starship ever"}
+        }
+    ]
+    return render_template("index.html", title = "Starship SN15 wont RUD", posts = posts)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8081', debug=True)
