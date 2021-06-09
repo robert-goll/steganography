dee676fc8bc0b079011c166e5ee14b37196e320e
robertrover811 <robertrover811@outlook.com>
2021-06-09 14:58:54 -0700

    Added the posts demo

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,3 +9,12 @@ class User(db.Model):
 
     def __repr__(self):
         return f"user {self.username}"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    userid = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    def __repr__(self):
+        return f"post {self.body}"
