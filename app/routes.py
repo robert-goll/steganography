1d4c8e40678d8ca3890e5a72100cd73ab3356325
robertrover811 <robertrover811@outlook.com>
2021-07-07 15:21:09 -0700

    finished login for now

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from flask.helpers import url_for
 from flask_login.utils import login_required, logout_user
+from werkzeug.urls import url_parse
 from app import app
 from flask import Flask, request, render_template, flash, redirect
 # from Steganography import app
 from forms import LoginForm
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, login_required
 from app.models import User
 
 @app.route("/login", methods=["GET", "POST"])
@@ -13,13 +14,16 @@ def login():
         return redirect(url_for("index"))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username = form.username.data().first())
-        if user is None or not user.check_password(form.password.data()):
-            flash(f"Invalid Username or Password")
-            return redirect(url_for("login"))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for("index"))
-    return render_template("login.html", title = "Sign In", form = form)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template("login.html", title="Sign In", form=form)
 
 @app.route("/logout")
 def logout():
@@ -27,7 +31,7 @@ def logout():
     return redirect(url_for("index"))
 
 @app.route("/", methods=["GET"])
-@app.route("/home", methods=["GET"])
-# @login_required left off here
+@app.route("/index", methods=["GET"])
+@login_required
 def index():
     return render_template("index.html", title="Sign In")
