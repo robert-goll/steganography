54c7fe39c0c9b5b60598acfec7f399c2efe5d00c
robertrover811 <robertrover811@outlook.com>
2021-07-07 15:56:39 -0700

    register works
    email doesnt send tho

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,26 @@
 from flask.helpers import url_for
 from flask_login.utils import login_required, logout_user
 from werkzeug.urls import url_parse
-from app import app
+from app import app, db
 from flask import Flask, request, render_template, flash, redirect
 # from Steganography import app
-from forms import LoginForm
+from forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, login_required
 from app.models import User
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
